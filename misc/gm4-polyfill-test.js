68dea88daa5c9b70271f44575cbdbdb7be772833
Eselce <S.Loges@gmx.de>
2017-11-14 10:01:49 +0100

    Test for RMC

---
+++ misc/gm4-polyfill-test.js
@@ -0,0 +1,132 @@
+/*
+This helper script bridges compatibility between the Greasemonkey 4 APIs and
+existing/legacy APIs.  Say for example your user script includes
+
+    // @grant GM_getValue
+
+And you'd like to be compatible with both Greasemonkey 3 and Greasemonkey 4
+(and for that matter all versions of Violentmonkey, Tampermonkey, and any other
+user script engine).  Add:
+
+    // @grant GM.getValue
+    // @require https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
+
+And switch to the new (GM-dot) APIs, which return promises.  If your script
+is running in an engine that does not provide the new asynchronous APIs, this
+helper will add them, based on the old APIs.
+
+If you use `await` at the top level, you'll need to wrap your script in an
+`async` function to be compatible with any user script engine besides
+Greasemonkey 4.
+
+    (async () => {
+    let x = await GM.getValue('x');
+    })();
+*/
+
+console.log("Init GM4-Polyfill...");
+if (typeof GM == 'undefined') {
+  GM = {};
+}
+
+
+if (typeof GM_addStyle == 'undefined') {
+  this.GM_addStyle = (aCss) => {
+    'use strict';
+    let head = document.getElementsByTagName('head')[0];
+    if (head) {
+      let style = document.createElement('style');
+      style.setAttribute('type', 'text/css');
+      style.textContent = aCss;
+      head.appendChild(style);
+      return style;
+    }
+    return null;
+  };
+}
+
+
+if (typeof GM_registerMenuCommand == 'undefined') {
+  this.GM_registerMenuCommand = (caption, commandFunc, accessKey) => {
+    if (!document.body) {
+      console.error('GM_registerMenuCommand got no body.');
+      return;
+    }
+    let contextMenu = document.body.getAttribute('contextmenu');
+    let menu = (contextMenu ? document.querySelector('menu#' + contextMenu) : null);
+    if (!menu) {
+      menu = document.createElement('menu');
+      menu.setAttribute('id', 'gm-registered-menu');
+      menu.setAttribute('type', 'context');
+      document.body.appendChild(menu);
+      document.body.setAttribute('contextmenu', 'gm-registered-menu');
+    }
+    let menuItem = document.createElement('menuitem');
+    menuItem.textContent = caption;
+    menuItem.addEventListener('click', commandFunc, true);
+    menu.appendChild(menuItem);
+  };
+}
+
+
+  this.GM_registerVerbose = (caption, commandFunc, accessKey) => {
+    if (!document.body) {
+      console.error('GM_registerMenuCommand got no body.');
+      return;
+    }
+    let contextMenu = document.body.getAttribute('contextmenu');
+    let menu = (contextMenu ? document.querySelector('menu#' + contextMenu) : null);
+    console.log("Found contextMenu = ", contextMenu, " => ", menu);
+    if (!menu) {
+      menu = document.createElement('menu');
+      menu.setAttribute('id', 'gm-registered-menu');
+      menu.setAttribute('type', 'context');
+      document.body.appendChild(menu);
+      document.body.setAttribute('contextmenu', 'gm-registered-menu');
+      console.log("Created contextMenu = ", document.body.getAttribute('contextmenu'), " => ", menu);
+    }
+    let menuItem = document.createElement('menuitem');
+    menuItem.textContent = caption;
+    menuItem.addEventListener('click', commandFunc, true);
+    menu.appendChild(menuItem);
+    console.log("Created menuItem = ", menuItem.textContent, menuItem, " => ", menu);
+  };
+
+
+Object.entries({
+  'log': console.log,
+  'info': GM_info,
+}).forEach(([newKey, old]) => {
+  if (old && (typeof GM[newKey] == 'undefined')) {
+    GM[newKey] = old;
+  }
+});
+
+
+Object.entries({
+  'GM_addStyle': 'addStyle',
+  'GM_deleteValue': 'deleteValue',
+  'GM_getResourceURL': 'getResourceUrl',
+  'GM_getValue': 'getValue',
+  'GM_listValues': 'listValues',
+  'GM_notification': 'notification',
+  'GM_openInTab': 'openInTab',
+  'GM_registerMenuCommand': 'registerMenuCommand',
+  'GM_registerVerbose': 'GM_registerVerbose',
+  'GM_setClipboard': 'setClipboard',
+  'GM_setValue': 'setValue',
+  'GM_xmlhttpRequest': 'xmlHttpRequest',
+}).forEach(([oldKey, newKey]) => {
+  let old = this[oldKey];
+  if (old && (typeof GM[newKey] == 'undefined')) {
+    GM[newKey] = () => {
+      return new Promise((resolve, reject) => {
+        try {
+          resolve(old.apply(this, arguments));
+        } catch (e) {
+          reject(e);
+        }
+      });
+    };
+  }
+});
---
+++ misc/gm4-polyfill.test.user.js
@@ -0,0 +1,62 @@
+// ==UserScript==
+// @name         gm4-polyfill.test
+// @namespace    http://wiki.greasespot.net/
+// @version      0.10
+// @copyright    2017+
+// @author       Sven Loges (SLC)
+// @description  RMC-Test-Script for gm4-polyfill and Greasemonkey 4.0
+// @include      /^https?://wiki\.greasespot\.net/Greasemonkey_Manual(#\S+)?(\?\S+(&\S+)*)?$/
+// @grant        GM.registerMenuCommand
+// @require      https://eselce.github.io/GitTest/misc/gm4-polyfill-test.js
+// @grant        GM_registerMenuCommand
+// ==/UserScript==
+
+// ECMAScript 6:
+/* jshint esnext: true */
+/* jshint moz: true */
+
+(() => {
+    function registerMenuCommand(text, fun, key) {
+        console.error('registerMenuCommand("' + text + '", ' + fun + ", '" + key + "')");
+
+        return GM.registerMenuCommand(text, fun, key);
+    }
+
+    function registerVerbose(text, fun, key) {
+        console.error('registerVerbose("' + text + '", ' + fun + ", '" + key + "')");
+
+        return GM.registerMenuCommand(text, fun, key);
+    }
+
+    // ==================== Hauptprogramm ====================
+
+    function procMain() {
+        Object.entries({
+            'Eins' : 1,
+            'Zwei' : 4,
+            'Drei' : 9,
+            'Vier' : 16,
+            'Fuenf' : 25
+        }).forEach(([oldKey, newKey]) => {
+            registerVerbose(oldKey, () => {
+                console.error(oldKey + " hoch 2 ist " + newKey);
+            }, oldKey.substr(0, 1));
+        });
+
+        Object.entries({
+            'Eins' : 1,
+            'Zwei' : 4,
+            'Drei' : 9,
+            'Vier' : 16,
+            'Fuenf' : 25
+        }).forEach(([oldKey, newKey]) => {
+            registerVerbose('Alt-' + oldKey, () => {
+                console.error(oldKey + " hoch 2 ist " + newKey);
+            }, oldKey.substr(1, 1));
+        });
+    }
+
+    procMain();
+})();
+
+// *** EOF ***
