ec64dc064c07331c90a0e367cdc59f53da1cd2a4
SLC <S.Loges@gmx.de>
2021-10-07 05:11:33 +0200

    Angabe der Fehlersituation im Fehlerfall

--- misc/OS2/lib/test.assert.js
+++ misc/OS2/lib/test.assert.js
@@ -18,20 +18,25 @@
 // ==================== Abschnitt fuer Klasse AssertionFailed ====================
 
 // Basisklasse fuer eine spezielle Exception fuer Assertions
+// whatFailed: Info, was schief lief
 // msg: Text oder Text liefernde Funktion
 // thisArg: Referenz auf ein Bezugsobjekt
 // params: ggfs. Parameter fuer die msg-Funktion
-function AssertionFailed(msg, thisArg, ...params) {
+function AssertionFailed(whatFailed, msg, thisArg, ...params) {
     //'use strict';
 
     if (msg === undefined) {
-        this.text = msg;
+        this.text = "";
     } else if (typeof msg === 'function') {
         const __TEXT = msg.call(thisArg, ...params);
 
         this.text = ((__TEXT === undefined) ? __TEXT : String(__TEXT));
     } else {
         this.text = String(msg);
+    }
+
+    if (whatFailed) {
+        this.text += " (" + whatFailed + ')';
     }
 }
 
@@ -47,16 +52,17 @@ Class.define(AssertionFailed, Object, {
 
 // Basisfunktion fuer die Durchfuehrung einer Ueberpruefung einer Bedingung
 // test: Bedingung, die als wahr angenommen wird
+// whatFailed: Info, was schief lief fuer den Fall einer Diskrepanz
 // msg: Text oder Text liefernde Funktion fuer den Fall einer Diskrepanz
 // thisArg: Referenz auf ein Bezugsobjekt
 // params: ggfs. Parameter fuer die msg-Funktion
 // throw Wirft dann AssertionFailed-Exception mit diesem Text
 // return true, da in diesem Fall keine Exception geworfen wurde!
-const ASSERT = function(test, msg, thisArg, ...params) {
+const ASSERT = function(test, whatFailed, msg, thisArg, ...params) {
     //'use strict';
 
     if (! test) {
-        const __FAIL = new AssertionFailed(msg, thisArg, ...params);
+        const __FAIL = new AssertionFailed(whatFailed, msg, thisArg, ...params);
         __LOG[3]("FAIL", __FAIL);
 
         throw __FAIL;
@@ -67,68 +73,85 @@ const ASSERT = function(test, msg, thisArg, ...params) {
     return true;
 };
 
+
+// Basisfunktion fuer die Durchfuehrung einer Ueberpruefung einer Fehler-Bedingung
+// test: Bedingung, die als falsch angenommen wird
+// whatFailed: Info, was schief lief fuer den Fall einer Uebereinstimmung
+// msg: Text oder Text liefernde Funktion fuer den Fall einer Uebereinstimmung
+// thisArg: Referenz auf ein Bezugsobjekt
+// params: ggfs. Parameter fuer die msg-Funktion
+// throw Wirft dann AssertionFailed-Exception mit diesem Text
+// return true, da in diesem Fall keine Exception geworfen wurde!
+const ASSERT_NOT = function(test, whatFailed, msg, thisArg, ...params) {
+    return ASSERT(! test, whatFailed, msg, thisArg, ...params);
+}
+
 // ==================== Ende Abschnitt fuer ASSERT ====================
 
 // ==================== Abschnitt fuer sonstige ASSERT-Funktionen ====================
 
-const ASSERT_NOT = function(test, msg, thisArg, ...params) {
-    return ASSERT(! test, msg, thisArg, ...params);
+const ASSERT_TRUE = function(test, msg, thisArg, ...params) {
+    return ASSERT(test, "false", msg, thisArg, ...params);
+}
+
+const ASSERT_NOT_TRUE = function(test, msg, thisArg, ...params) {
+    return ASSERT(! test, "true", msg, thisArg, ...params);
 }
 
 const ASSERT_NULL = function(test, msg, thisArg, ...params) {
-    return ASSERT(test === null, msg, thisArg, ...params);
+    return ASSERT(test === null, test + " !== null", msg, thisArg, ...params);
 }
 
 const ASSERT_NOT_NULL = function(test, msg, thisArg, ...params) {
-    return ASSERT(test !== null, msg, thisArg, ...params);
+    return ASSERT(test !== null, test + " === null", msg, thisArg, ...params);
 }
 
 const ASSERT_SET = function(test, msg, thisArg, ...params) {
-    return ASSERT(test != undefined, msg, thisArg, ...params);
+    return ASSERT(test != undefined, test + " == undefined", msg, thisArg, ...params);
 }
 
 const ASSERT_NOT_SET = function(test, msg, thisArg, ...params) {
-    return ASSERT(test == undefined, msg, thisArg, ...params);
+    return ASSERT(test == undefined, test + " != undefined", msg, thisArg, ...params);
 }
 
 const ASSERT_EQUAL = function(erg, exp, msg, thisArg, ...params) {
-    return ASSERT(erg === exp, msg, thisArg, ...params);
+    return ASSERT(erg === exp, erg + " !== " + exp, msg, thisArg, ...params);
 }
 
 const ASSERT_NOT_EQUAL = function(erg, exp, msg, thisArg, ...params) {
-    return ASSERT(erg !== exp, msg, thisArg, ...params);
+    return ASSERT(erg !== exp, erg + " === " + exp, msg, thisArg, ...params);
 }
 
 const ASSERT_ALIKE = function(erg, exp, msg, thisArg, ...params) {
-    return ASSERT(erg == exp, msg, thisArg, ...params);
+    return ASSERT(erg == exp, erg + " != " + exp, msg, thisArg, ...params);
 }
 
 const ASSERT_NOT_ALIKE = function(erg, exp, msg, thisArg, ...params) {
-    return ASSERT(erg != exp, msg, thisArg, ...params);
+    return ASSERT(erg != exp, erg + " == " + exp, msg, thisArg, ...params);
 }
 
 const ASSERT_IN_DELTA = function(erg, exp, delta, msg, thisArg, ...params) {
-    return ASSERT(Math.abs(erg - exp) <= delta, msg, thisArg, ...params);
+    return ASSERT(Math.abs(erg - exp) <= delta, erg + " != " + exp + " +/- " + delta, msg, thisArg, ...params);
 }
 
 const ASSERT_NOT_IN_DELTA = function(erg, exp, delta, msg, thisArg, ...params) {
-    return ASSERT(Math.abs(erg - exp) > delta, msg, thisArg, ...params);
+    return ASSERT(Math.abs(erg - exp) > delta, erg + " == " + exp + " +/- " + delta, msg, thisArg, ...params);
 }
 
 const ASSERT_INSTANCEOF = function(obj, cls, msg, thisArg, ...params) {
-    return ASSERT((obj instanceof cls), msg, thisArg, ...params);
+    return ASSERT((obj instanceof cls), obj + " ist kein " + cls, msg, thisArg, ...params);
 }
 
 const ASSERT_NOT_INSTANCEOF = function(obj, cls, msg, thisArg, ...params) {
-    return ASSERT_NOT((obj instanceof cls), msg, thisArg, ...params);
+    return ASSERT_NOT((obj instanceof cls), obj + " ist " + cls, msg, thisArg, ...params);
 }
 
 const ASSERT_MATCH = function(str, pattern, msg, thisArg, ...params) {
-    return ASSERT((str || "").match(pattern), msg, thisArg, ...params);
+    return ASSERT((str || "").match(pattern), str + " match-t " + pattern, msg, thisArg, ...params);
 }
 
 const ASSERT_NOT_MATCH = function(str, pattern, msg, thisArg, ...params) {
-    return ASSERT_NOT((str || "").match(pattern), msg, thisArg, ...params);
+    return ASSERT_NOT((str || "").match(pattern), str + " match-t nicht " + pattern, msg, thisArg, ...params);
 }
 
 // ==================== Ende Abschnitt fuer sonstige ASSERT-Funktionen ====================
