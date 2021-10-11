27552371106d5635e2063a16a640604b3b9f61d1
SLC <S.Loges@gmx.de>
2021-10-11 13:07:35 +0200

    test.assert.js: Objekt-Funktionen nach util.object.js ausgelagert

--- misc/OS2/lib/test.assert.js
+++ misc/OS2/lib/test.assert.js
@@ -7,7 +7,7 @@
 // _description  JS-lib mit ASSERT-Funktionen aller Art und AssertFailed
 // _require      https://eselce.github.io/OS2.scripts/lib/util.debug.js
 // _require      https://eselce.github.io/OS2.scripts/lib/util.class.js
-// _require      https://eselce.github.io/OS2.scripts/lib/test.class.unittest.js
+// _require      https://eselce.github.io/OS2.scripts/lib/util.object.js
 // _require      https://eselce.github.io/OS2.scripts/lib/test.assert.js
 // ==/UserScript==
 
@@ -72,7 +72,6 @@ const ASSERT = function(test, whatFailed, msg, thisArg, ...params) {
 
     return true;
 };
-
 
 // Basisfunktion fuer die Durchfuehrung einer Ueberpruefung einer Fehler-Bedingung
 // test: Bedingung, die als falsch angenommen wird
@@ -162,163 +161,5 @@ const ASSERT_NOT_MATCH = function(str, pattern, msg, thisArg, ...params) {
 const __ASSERTDELTA = 0.000001;
 
 // ==================== Ende Abschnitt fuer globale Variablen ====================
-
-// ==================== Abschnitt fuer detaillierte Ausgabe von Daten (auszulagern) ====================
-
-// Fuehrt eine Map-Function auf ein Object aus und liefert ein neues Objekt zurueck.
-// Zusaetzlich kann die Auswahl der Elemente per Filter eingeschraenkt werden sowie
-// das Ergebnis sortiert (Default nach Wert, aber auch nach Schluessel).
-// obj: Das Object, das gemappt wird
-// mapFun: Eine Mapping-Funktion (value [, key [, index [, array]]])
-// - value: Wert
-// - key: Schluessel
-// - index: lfd. Nummer des Eintrags
-// - array: entries() des Objekts obj
-// | Alternativ Ein zu setzender Wert (keine Funktion)
-// thisArg: Wert, der als this verwendet wird, wenn mapFun, filterFun und sortFun ausgeführt werden (Default: obj)
-// filterFun: Eine Filter-Funktion auf (value [, key [, index [, array]]]) (Default: alle Elemente)
-// - value: Wert
-// - key: Schluessel
-// - index: lfd. Nummer des Eintrags
-// - array: entries() des Objekts obj
-// | Alternativ undefined, null: alle Elemente
-// | Alternativ Ein (schwach "==") zu vergleichender Wert (keine Funktion)
-// sortFun: Eine Sortier-Funktion auf (value1, value2 [, key1, key2])
-// - value1: Erster Wert
-// - value2: Zweiter Wert
-// - key1: Erster Schluessel
-// - key2: Zweiter Schluessel
-// | Alternativ undefined, false: unsortiert
-// | Alternativ true: Normale Sortierung anhand der UTF-16 Codepoints
-// return Ein neues Object mit gemappten Werten
-Object.map = function(obj, mapFun, thisArg, filterFun, sortFun) {
-    if (! obj) {
-        __LOG[4]("Object.map():", "Keine Aktion bei leerem Objekt", obj);
-
-        return obj;
-    } else if ((typeof obj) === 'object') {
-        const __THIS = (thisArg || obj);
-        const __MAPFUN = (((typeof mapFun) === 'function')
-                          ? (([key, value], index) => [key, mapFun.call(__THIS, value, key, index, __FILTERARR)])
-                          : (([key, value]) => [key, mapFun]));
-        const __FILTERFUN = ((filterFun == undefined)
-                             ? (element => true)
-                             : (((typeof filterFun) === 'function')
-                                ? (([key, value], index) => [key, filterFun.call(__THIS, value, key, index, __ARR)])
-                                : (([key, value]) => (value == filterFun))));
-        const __SORTFUN = ((sortFun === true)
-                           ? undefined
-                           : (([key1, value1], [key2, value2]) => sortFun.call(__THIS, value1, value2, key1, key2)));
-        const __ARR = Object.entries(obj);
-        const __FILTERARR = __ARR.filter(__FILTERFUN);  // [, __THIS] wird bereits erledigt
-        const __MAPPEDARR = __FILTERARR.map(__MAPFUN);  // [, __THIS] wird bereits erledigt
-
-        return Object.fromEntries(((sortFun) ? __MAPPEDARR.sort(__SORTFUN) : __MAPPEDARR));
-    } else {
-        __LOG[1]("Object.map():", "Illegales Objekt erhalten", obj);
-
-        return obj;
-    }
-}
-
-//
-// Hilfsfunktionen, die von Options.toString() genutzt werden
-//
-
-function getClass(obj) {
-    if (obj != undefined) {
-        if (typeof obj === 'object') {
-            if (obj.getClass) {
-                return obj.getClass();
-            }
-        }
-    }
-
-    return undefined;
-}
-
-function getClassName(obj) {
-    const __CLASS = getClass(obj);
-
-    return ((__CLASS ? __CLASS.className : undefined));  // __CLASS.getName() problematisch?
-}
-
-function getObjInfo(obj, keyStrings, longForm, stepIn) {
-    const __TYPEOF = typeof obj;
-    const __VALUEOF = Object.valueOf(obj);
-    const __LENGTH = ((obj != undefined) ? ((__TYPEOF === 'object') ? Object.entries(obj) : obj).length : obj);
-    const __STRDELIM1 = (keyStrings ? "'" : '"');
-    const __STRDELIM2 = (keyStrings ? "'" : '"');
-    const __NUMDELIM1 = (keyStrings ? "" : '<');
-    const __NUMDELIM2 = (keyStrings ? "" : '>');
-    const __SPACE = (keyStrings ? "" : ' ');
-    const __ARRDELIM = ',' + __SPACE;
-    const __ARRDELIM1 = '[';
-    const __ARRDELIM2 = ']';
-    const __OBJSETTER = __SPACE + ':' + __SPACE;
-    const __OBJDELIM = ',' + __SPACE;
-    const __OBJDELIM1 = '{';
-    const __OBJDELIM2 = '}';
-    const __LENSTR = ((__LENGTH !== undefined) ? __ARRDELIM1 + __LENGTH + __ARRDELIM2 : "")
-    const __VALUESTR = String(obj);
-    let typeStr = __TYPEOF;
-    let valueStr = __VALUESTR;
-
-    switch (__TYPEOF) {
-    case 'undefined' : break;
-    case 'string'    : typeStr = 'String';
-                       valueStr = __STRDELIM1 + valueStr + __STRDELIM2;
-                       break;
-    case 'boolean'   : typeStr = 'Boolean';
-                       break;
-    case 'number'    : if (Number.isInteger(obj)) {
-                           typeStr = 'Integer';
-                       } else {
-                           typeStr = 'Number';
-                           valueStr = __NUMDELIM1 + valueStr + __NUMDELIM2;
-                       }
-                       break;
-    case 'function'  : break;
-    case 'object'    : if (Array.isArray(obj)) {
-                           const __VALSTR = (__LENGTH ? obj.map(item => getValStr(item, false, stepIn, longForm, stepIn)).join(__ARRDELIM) : "");
-
-                           typeStr = 'Array';
-                           valueStr = __ARRDELIM1 + (__LENGTH ? __SPACE + __VALSTR + __SPACE : "") + __ARRDELIM2;
-                       } else {
-                           const __CLASS = getClass(obj);
-                           const __CLASSNAME = (__CLASS ? getClassName(obj) : "");
-                           const __VALSTR = (__LENGTH ? Object.values(Object.map(obj, (value, key) => (getValStr(key, true) + __OBJSETTER
-                                            + getValStr(value, false, stepIn, longForm, stepIn)))).join(__OBJDELIM) : "");
-
-                           typeStr = (__CLASSNAME ? __CLASSNAME : typeStr);
-                           valueStr = (__CLASSNAME ? __CLASSNAME + __SPACE : "") + __OBJDELIM1 + (__LENGTH ? __SPACE + __VALSTR + __SPACE : "") + __OBJDELIM2;
-                       }
-    default :          break;
-    }
-
-    if (obj == undefined) {
-        if (obj === undefined) {  // sic!
-            valueStr = "";
-        } else {  // null o.ä.
-            valueStr = __VALUESTR;
-        }
-    }
-
-    return [
-               typeStr + (longForm ? __LENSTR : ""),
-               valueStr,
-               __TYPEOF,
-               __VALUEOF,
-               __LENGTH
-           ];
-}
-
-function getValStr(obj, keyStrings, showType, showLen, stepIn) {
-    const [ __TYPESTR, __VALUESTR ] = getObjInfo(obj, keyStrings, showLen, stepIn);
-
-    return (showType ? __TYPESTR + ' ' : "") + __VALUESTR;
-}
-
-// ==================== Abschnitt fuer detaillierte Ausgabe von Daten (auszulagern) ====================
 
 // *** EOF ***
