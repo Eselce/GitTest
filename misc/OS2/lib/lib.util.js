5007eb0dd650dd84c5343885ded843184c4e581c
SLC <S.Loges@gmx.de>
2021-11-19 07:44:11 +0100

    Update der Bibliotheken

--- misc/OS2/lib/lib.util.js
+++ misc/OS2/lib/lib.util.js
@@ -11,6 +11,8 @@
 //  util.mem.mod.js
 //  util.debug.js
 //  util.store.js
+//  util.xhr.js
+//  util.xhr.gm.js
 //  util.dom.js
 //  util.script.js
 
@@ -1657,6 +1659,338 @@ function setNextStored(arr, name, value, reload = false, serial = false, onFulfi
 // *** EOF ***
 
 /*** Ende Modul util.store.js ***/
+
+/*** Modul util.xhr.js ***/
+
+// ==UserScript==
+// _name         util.xhr
+// _namespace    http://os.ongapo.com/
+// _version      0.10
+// _copyright    2017+
+// _author       Sven Loges (SLC)
+// _description  JS-lib mit Funktionen und Utilities fuer XHR-Aufrufe
+// _require      https://eselce.github.io/OS2.scripts/lib/util.log.js
+// _require      https://eselce.github.io/OS2.scripts/lib/util.xhr.js
+// ==/UserScript==
+
+// ECMAScript 6:
+/* jshint esnext: true */
+/* jshint moz: true */
+
+// ==================== Default XHR-Handler ====================
+
+// Der XHR-Handler...
+const __XHR = XHRfactory('XHR handler', XMLHttpRequest, openXMLHttpRequest);
+
+// openXMLHttpRequest(XHRrequest, details): Aufbau einer Verbindung ueber Verbindungsdetails
+// XHRrequest: Verbindungs-Objekt
+// details: Verbindungsdetails zur Verbindung
+// - method: Art der Verbindung ('GET', 'POST', 'PUT', 'HEAD', 'DELETE'...)
+// - url: Ziel der Verbindung
+// - onload: Event-Listener fuer das Laden
+// - async: true/false, ob asynchrone Verbindung
+// - ...
+// return Antwort-Promise auf die Verbindung
+function openXMLHttpRequest(XMLrequest, details) {
+    const __REQUEST = XMLrequest;
+    const __D = details;
+
+    // TODO klaeren!
+    __D.async = false;
+
+    __REQUEST.addEventListener('load', __D.onload);
+    __REQUEST.open(__D.method, __D.url, __D.async, __D.user, __D.password);
+    __REQUEST.send(__D.data);
+
+    return __REQUEST;
+}
+
+// ==================== Ende Default XHR-Handler ====================
+
+// ==================== Fabrik fuer einen XHR-Handler ====================
+
+// Fabrik fuer die Generierung eines XHR-Handlers
+// XHRname: Name des XHR-Handlers
+// XHRrequestClass: Klasse des Verbindungs-Objekts eines XHR-Requests
+// XHRopenFun(XHRrequest, details): Aufbau einer Verbindung ueber Verbindungsdetails
+// - XHRrequest: Verbindungs-Objekt
+// - details: Verbindungsdetails zur Verbindung
+// - * method: Art der Verbindung ('GET', 'POST', 'PUT', 'HEAD', 'DELETE'...)
+// - * url: Ziel der Verbindung
+// - * onload: Event-Listener fuer das Laden
+// - * async: true/false, ob asynchrone Verbindung
+// - * ...
+// - return Antwort-Promise auf die Verbindung
+// return Liefert ein Handler-Objekt mit folgenden Methoden:
+// - browse(url, headers, onload): Einfacher Aufbau einer Verbindung
+// - getRequest(details): Aufbau einer Verbindung mit GET-Headern
+// - putRequest(details): Aufbau einer Verbindung mit PUT-Headern
+// - xmlRequest(details): Allgemeiner Aufbau einer Verbindung
+// - postRequest(details): Aufbau einer Verbindung mit POST-Headern
+// - headRequest(details): Aufbau einer Verbindung mit HEAD-Headern
+// - registerCallback(rc, fun): Registrierung fuer Behandlung verschiedener Response-Codes
+// - __XMLREQUEST: Klasse des Verbindungs-Objekts, siehe XHRrequestClass
+function XHRfactory(XHRname, XHRrequestClass, XHRopenFun) {
+    if ((typeof XHRopenFun) === 'function') {
+        __LOG[2]("Initializing", XHRname, '...');
+    } else {
+        throw TypeError("Can't initialize " + XHRname + " with " + __LOG.info(XHRopenFun) + '!');
+    }
+
+    const __XMLREQUEST = XHRrequestClass;
+
+    const __DETAILS = {
+        'GET'     : {
+                        'method' : 'GET'
+                    },
+        'PUT'     : {
+                        'method' : 'PUT'
+                    },
+        'POST'    : {
+                        'method' : 'POST'
+                    },
+        'HEAD'    : {
+                        'method' : 'HEAD'
+                    }
+        };
+
+    const __HEADERS = {
+        'FORM'    : {
+                        'Content-Type'    : 'application/x-www-form-urlencoded; charset=UTF-8'
+                    },
+        'ACC'     : {
+                        'Accept'          : "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+                        'Accept-Language' : "de,en;q=0.9,fr;q=0.7,ja;q=0.6,zh;q=0.4,zh-CN;q=0.3,en-US;q=0.1",
+                        'Accept-Encoding' : "gzip, deflate, br"
+                    },
+        'FF95'    : {
+                        'User-Agent'      : 'Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:95.0) Gecko/20100101 Firefox/95.0'
+                    }
+        };
+
+    const __CALLBACKS = { };
+
+    function registerCallback(status, fun) {
+        __CALLBACKS[status] = fun;
+    }
+
+    function runCallback(result) {
+        const __CALLBACK = __CALLBACKS[result.status];
+
+        __LOG[2](result.status, __CALLBACK);
+
+        return (__CALLBACK ? __CALLBACK(result) : null);
+    }
+
+    function onloadByStatus(result) {
+        __LOG[2](result.status, result.statusText);
+
+        return runCallback(result);
+    }
+
+    function xmlRequest(details) {
+        return new Promise(function(resolve, reject) {
+                const __ONLOAD = details.onload;
+                const __D = { };
+
+                Object.assign(__D, details);
+
+                __D.onload = (result => {
+                        const __RESULT = (result.target || result);
+                        const __RET = __ONLOAD(__RESULT);
+
+                        if (__RESULT.statusText === 'OK') {
+                            resolve(__RET);
+                        } else {
+                            reject(__RESULT.statusText);
+                        }
+                    });
+
+                const __REQUEST = new __XMLREQUEST();
+
+                if (__REQUEST) {
+                    __LOG[2]('Fetching', details.url, '...');
+
+                    const __RET = XHRopenFun(__REQUEST, __D);
+
+                    if (__RET !== undefined) {
+                        return __RET;
+                    }
+                }
+
+                __LOG[1]('Tried to fetch', details.url, '...');
+
+                reject(XHRname + " is missing!");
+            });
+    }
+
+    function getRequest(details) {
+        const __D = { };
+
+        Object.assign(__D, details, __DETAILS.GET);
+
+        const __RET = xmlRequest(__D);
+
+        return __RET;
+    }
+
+    function putRequest(details) {
+        const __H = { };
+        const __D = { };
+
+        Object.assign(__H, __HEADERS.FORM, __HEADERS.ACC, __HEADERS.FF95, details.headers);
+        Object.assign(__D, details, { 'headers' : __H }, __DETAILS.PUT);
+
+        const __RET = xmlRequest(__D);
+
+        return __RET;
+    }
+
+    function postRequest(details) {
+        const __H = { };
+        const __D = { };
+
+        Object.assign(__H, __HEADERS.FORM, __HEADERS.ACC, __HEADERS.FF95, details.headers);
+        Object.assign(__D, details, { 'headers' : __H }, __DETAILS.PUT);
+
+        const __RET = xmlRequest(__D);
+
+        return __RET;
+    }
+
+    function headRequest(details) {
+        const __D = { };
+
+        Object.assign(__D, details, __DETAILS.HEAD);
+
+        const __RET = xmlRequest(__D);
+
+        return __RET;
+    }
+
+    function browse(url, headers = { }, onload = onloadByStatus) {
+        const __H = { };
+
+        Object.assign(__H, __HEADERS.ACC, __HEADERS.FF95, headers);
+
+        return getRequest({
+                'url'     : url,
+                'headers' : __H,
+                'onload'  : onload
+            });
+    }
+
+    registerCallback(200, function(result) {
+            let parser = new DOMParser();
+            let contentType;
+            let doc;
+
+            try {
+                if (result.responseHeaders) {
+                    let match = result.responseHeaders.match(/^Content-Type:\s+((\S+)\/(\S+))$/m);
+                    contentType = (match ? match[1] : 'application/xml');
+                    __LOG[2](contentType);
+
+                    doc = parser.parseFromString(result.responseText, contentType);
+
+                    __LOG[5]("Parsed:", doc);
+                } else {
+                    __LOG[5]("Raw document", (result.responseType ? result.responseType : "*/*"));
+
+                    doc = result.response;
+
+                    //__LOG[6]("Parsed:", doc.slice(0, 256), '\n...\n', doc.slice(-256));
+                }
+            }
+            catch(ex) {
+                __LOG[1]("Parse error:", ex);
+            }
+
+            return doc;
+        });
+
+    return {
+            browse,
+            getRequest,
+            putRequest,
+            postRequest,
+            headRequest,
+            xmlRequest,
+            registerCallback,
+            __XMLREQUEST
+        };
+}
+
+// ==================== Ende Fabrik fuer einen XHR-Handler ====================
+
+// *** EOF ***
+
+/*** Ende Modul util.xhr.js ***/
+
+/*** Modul util.xhr.gm.js ***/
+
+// ==UserScript==
+// _name         util.xhr.gm
+// _namespace    http://os.ongapo.com/
+// _version      0.10
+// _copyright    2017+
+// _author       Sven Loges (SLC)
+// _description  JS-lib mit Funktionen und Utilities fuer GM XHR-Aufrufe
+// _require      https://eselce.github.io/OS2.scripts/lib/util.log.js
+// _require      https://eselce.github.io/OS2.scripts/lib/util.xhr.js
+// _require      https://eselce.github.io/OS2.scripts/lib/util.xhr.gm.js
+// ==/UserScript==
+
+// ECMAScript 6:
+/* jshint esnext: true */
+/* jshint moz: true */
+
+// ==================== Ermittlung der vorhandenen Greasemonkey Funktionen ====================
+
+const __GM3REQUEST = (('undefined' !== typeof GM_xmlhttpRequest) ? GM_xmlhttpRequest : undefined);  // GM 3.x and earlier
+const __GM4REQUEST = (('undefined' !== typeof GM)                ? GM.xmlHttpRequest : undefined);  // GM 4.0+
+const __CHECKFUN   = (fun => (('function' === typeof fun) ? fun : undefined));
+
+const __GMREQUEST = (__CHECKFUN(__GM4REQUEST) || __CHECKFUN(__GM3REQUEST));
+
+// ==================== Ende Ermittlung der vorhandenen Greasemonkey Funktionen ====================
+
+// ==================== Greasemonkey XHR-Handler ====================
+
+// Der Greasemonkey XHR-Handler...
+const __GM_XHR = XHRfactory('Greasemonkey XHR handler', GMXMLHttpRequest, openGMXMLHttpRequest);
+
+// openGMXMLHttpRequest(XHRrequest, details): Aufbau einer Verbindung ueber Verbindungsdetails
+// XHRrequest: Verbindungs-Objekt
+// details: Verbindungsdetails zur Verbindung
+// - method: Art der Verbindung ('GET', 'POST', 'PUT', 'HEAD', 'DELETE'...)
+// - url: Ziel der Verbindung
+// - onload: Event-Listener fuer das Laden
+// - async: true/false, ob asynchrone Verbindung
+// - ...
+// return Antwort-Promise auf die Verbindung
+async function openGMXMLHttpRequest(XMLrequest, details) {
+    const __REQUEST = XMLrequest;
+    const __D = details;
+
+    __REQUEST;
+
+    return await __GMREQUEST(__D);
+}
+
+// ==================== Ende Default XHR-Handler ====================
+
+// ==================== Abschnitt fuer Klasse GMXMLHttpRequest ====================
+
+// Eventuell fuer spaetere Erweiterungen: Klasse des Verbindungs-Objekts eines XHR-Requests...
+// Bisher leer, da GM.xmlHttpRequest(details) in der openGMXMLHttpRequest()-Funktion bereits alles erledigt!
+function GMXMLHttpRequest() { }
+
+// ==================== Ende Abschnitt fuer Klasse GMXMLHttpRequest ====================
+
+// *** EOF ***
+
+/*** Ende Modul util.xhr.gm.js ***/
 
 /*** Modul util.dom.js ***/
 
