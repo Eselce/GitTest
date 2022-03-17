2b3dfc0ec14fa8be3b7cbb8a022f5a1e4334aaf2
SLC <S.Loges@gmx.de>
2022-03-17 09:41:21 +0100

    Erste Version OS2.saisonhistorie
    Steuerung der Rahmen-Stile
    Leeren der "Nicht teilgenommen"-Felder

---
+++ misc/OS2/OS2.saisonhistorie.user.js
@@ -0,0 +1,392 @@
+// ==UserScript==
+// @name         OS2.saisonhistorie
+// @namespace    http://os.ongapo.com/
+// @version      0.10+lib
+// @copyright    2022+
+// @author       Sven Loges (SLC)
+// @description  Saisonhistorie-Abschnitt aus dem Master-Script fuer Online Soccer 2.0
+// @include      /^https?://(www\.)?(os\.ongapo\.com|online-soccer\.eu|os-zeitungen\.com)/(st|showteam)\.php\?s=10(&\w+=?[+\w]+)*(#\w+)?$/
+// @grant        GM.getValue
+// @grant        GM.setValue
+// @grant        GM.deleteValue
+// @grant        GM.registerMenuCommand
+// @grant        GM.info
+// @require      https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_deleteValue
+// @grant        GM_registerMenuCommand
+// @grant        GM_info
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/util.log.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/util.object.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/util.value.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/util.proto.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/util.prop.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/util.mem.sys.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/util.mem.mod.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/util.debug.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/util.store.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/util.dom.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/util.class.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/util.class.delim.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/util.class.path.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/util.class.uri.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/util.option.type.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/util.option.data.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/util.option.class.options.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/util.option.api.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/util.mem.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/util.mem.db.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/util.mem.cmd.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/util.option.menu.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/util.option.page.label.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/util.option.page.action.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/util.option.page.node.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/util.option.page.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/util.option.run.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/util.main.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/OS2.list.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/OS2.team.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/OS2.page.team.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/OS2.page.js
+// @require      https://eselce.github.io/GitTest/misc/OS2/lib/OS2.zat.js
+// ==/UserScript==
+
+// ECMAScript 6:
+/* jshint esnext: true */
+/* jshint moz: true */
+
+// ==================== Konfigurations-Abschnitt fuer Optionen ====================
+
+const __LOGLEVEL = 4;
+
+// Moegliche Optionen (hier die Standardwerte editieren oder ueber das Benutzermenu setzen):
+const __OPTCONFIG = {
+    'longStats' : {       // Europapokal ausfuehrlich
+                   'Name'      : "longStats",
+                   'Type'      : __OPTTYPES.SW,
+                   'Default'   : false,
+                   'Action'    : __OPTACTION.NXT,
+                   'Label'     : "Lange Stats",
+                   'Hotkey'    : 'L',
+                   'AltLabel'  : "Kurze Stats",
+                   'AltHotkey' : 'K',
+                   'FormLabel' : "Lange Stats"
+               },
+    'showStats' : {       // Alle Titel und Erfolge zusammenfassen
+                   'Name'      : "showStats",
+                   'Type'      : __OPTTYPES.SW,
+                   'Default'   : true,
+                   'Action'    : __OPTACTION.NXT,
+                   'Label'     : "Stats ein",
+                   'Hotkey'    : 'S',
+                   'AltLabel'  : "Stats aus",
+                   'AltHotkey' : 'S',
+                   'FormLabel' : "Zusammenfassung"
+               },
+    'shortKom' : {        // "Nicht teilgenommen" weglassen
+                   'Name'      : "shortKom",
+                   'Type'      : __OPTTYPES.SW,
+                   'Default'   : true,
+                   'Action'    : __OPTACTION.NXT,
+                   'Label'     : "Leere Eintr\xE4ge",
+                   'Hotkey'    : 'T',
+                   'AltLabel'  : "Nicht teilgenommen",
+                   'AltHotkey' : 'T',
+                   'FormLabel' : "Leere Eintr\xE4ge"
+               },
+    'land' : {            // Name des Landes
+                   'Name'      : "land",
+                   'Type'      : __OPTTYPES.SD,
+                   'ValType'   : 'String',
+                   'FreeValue' : true,
+                   'Hidden'    : true
+               },
+    'aktuelleSaison' : {  // Laufende Saison
+                   'Name'      : "aktuelleSaison",
+                   'Type'      : __OPTTYPES.MC,
+                   'ValType'   : 'Number',
+                   'FreeValue' : true,
+                   'SelValue'  : false,
+                   'Choice'    : [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 ],
+                   'Default'   : 18,
+                   'Hidden'    : true
+               },
+    'ligaSize' : {        // Ligengroesse
+                   'Name'      : "ligaSize",
+                   'Type'      : __OPTTYPES.MC,
+                   'ValType'   : 'Number',
+                   'AutoReset' : true,
+                   'Choice'    : [ 10, 18, 20 ],
+                   'Action'    : __OPTACTION.NXT,
+                   'Label'     : "Liga: $er",
+                   'Hotkey'    : 'i',
+                   'FormLabel' : "Liga:|$er"
+               },
+    'team' : {            // Datenspeicher fuer Daten des Erst- bzw. Zweitteams
+                   'Name'      : "team",
+                   'Type'      : __OPTTYPES.SD,
+                   'Hidden'    : false,
+                   'Serial'    : true,
+                   'Permanent' : true,
+                   'Default'   : undefined,  // new Team() // { 'Team' : undefined, 'Liga' : undefined, 'Land' : undefined, 'TmNr' : 0, 'LdNr' : 0, 'LgNr' : 0 }
+                   'Submit'    : undefined,
+                   'Cols'      : 36,
+                   'Rows'      : 6,
+                   'Replace'   : null,
+                   'Space'     : 1,
+                   'Label'     : "Verein:"
+               },
+    'landNr' : {          // ID des Landes
+                   'Name'      : "landNr",
+                   'Type'      : __OPTTYPES.SD,
+                   'ValType'   : 'Number',
+                   'FreeValue' : false,
+                   'SelValue'  : false,
+                   'Choice'    : Object.keys(__LAENDER),
+                   'Default'   : 0,
+                   'Hidden'    : true
+               },
+    'sepStyle' : {        // Stil der Trennlinie
+                   'Name'      : "sepStyle",
+                   'Type'      : __OPTTYPES.MC,
+                   'ValType'   : 'String',
+                   'Choice'    : [ 'solid', 'hidden', 'dotted', 'dashed', 'double', 'groove', 'ridge',
+                                   'inset', 'outset', 'none' ],
+                   'Action'    : __OPTACTION.NXT,
+                   'Label'     : "Stil: $",
+                   'Hotkey'    : 'i',
+                   'FormLabel' : "Stil:|$"
+               },
+    'sepColor' : {        // Farbe der Trennlinie
+                   'Name'      : "sepColor",
+                   'Type'      : __OPTTYPES.MC,
+                   'ValType'   : 'String',
+                   'FreeValue' : true,
+                   'Choice'    : [ 'white', 'yellow', 'black', 'blue', 'cyan', 'gold', 'grey', 'green',
+                                   'lime', 'magenta', 'maroon', 'navy', 'olive', 'orange', 'purple',
+                                   'red', 'teal', 'transparent' ],
+                   'Action'    : __OPTACTION.NXT,
+                   'Label'     : "Farbe: $",
+                   'Hotkey'    : 'F',
+                   'FormLabel' : "Farbe:|$"
+               },
+    'sepWidth' : {        // Dicke der Trennlinie
+                   'Name'      : "sepWidth",
+                   'Type'      : __OPTTYPES.MC,
+                   'ValType'   : 'String',
+                   'FreeValue' : true,
+                   'Choice'    : [ 'thin', 'medium', 'thick' ],
+                   'Action'    : __OPTACTION.NXT,
+                   'Label'     : "Dicke: $",
+                   'Hotkey'    : 'D',
+                   'FormLabel' : "Dicke:|$"
+               },
+    'reset' : {           // Optionen auf die "Werkseinstellungen" zuruecksetzen
+                   'FormPrio'  : undefined,
+                   'Name'      : "reset",
+                   'Type'      : __OPTTYPES.SI,
+                   'Action'    : __OPTACTION.RST,
+                   'Label'     : "Standard-Optionen",
+                   'Hotkey'    : 'O',
+                   'FormLabel' : ""
+               },
+    'storage' : {         // Browserspeicher fuer die Klicks auf Optionen
+                   'FormPrio'  : undefined,
+                   'Name'      : "storage",
+                   'Type'      : __OPTTYPES.MC,
+                   'ValType'   : 'String',
+                   'Choice'    : Object.keys(__OPTMEM),
+                   'Action'    : __OPTACTION.NXT,
+                   'Label'     : "Speicher: $",
+                   'Hotkey'    : 'c',
+                   'FormLabel' : "Speicher:|$"
+               },
+    'oldStorage' : {      // Vorheriger Browserspeicher fuer die Klicks auf Optionen
+                   'FormPrio'  : undefined,
+                   'Name'      : "oldStorage",
+                   'Type'      : __OPTTYPES.SD,
+                   'PreInit'   : true,
+                   'AutoReset' : true,
+                   'Hidden'    : true
+               },
+    'showForm' : {        // Optionen auf der Webseite (true = anzeigen, false = nicht anzeigen)
+                   'FormPrio'  : undefined,
+                   'Name'      : "showForm",
+                   'Type'      : __OPTTYPES.SW,
+                   'FormType'  : __OPTTYPES.SI,
+                   'Permanent' : true,
+                   'Default'   : false,
+                   'Title'     : "$V Optionen",
+                   'Action'    : __OPTACTION.NXT,
+                   'Label'     : "Optionen anzeigen",
+                   'Hotkey'    : 'a',
+                   'AltTitle'  : "$V schlie\xDFen",
+                   'AltLabel'  : "Optionen verbergen",
+                   'AltHotkey' : 'v',
+                   'FormLabel' : ""
+               }
+};
+
+// ==================== Ende Konfigurations-Abschnitt fuer Optionen ====================
+
+// ==================== Spezialisierter Abschnitt fuer Optionen ====================
+
+// Logging initialisieren mit Loglevel (siehe ganz oben im Konfigurationsabschnitt)...
+__LOG.init(window, __LOGLEVEL);
+
+// Teamparameter fuer getrennte Speicherung der Optionen fuer Erst- und Zweitteam...
+const __TEAMCLASS = new TeamClassification();
+
+// Optionen mit Daten, die ZAT- und Team-bezogen gemerkt werden...
+__TEAMCLASS.optSelect = {
+        'ligaSize'   : true
+    };
+
+// ==================== Ende Abschnitt fuer Optionen ====================
+
+// ==================== Page-Manager fuer zu bearbeitende Seiten ====================
+
+// Verarbeitet Ansicht "Saisonplan"
+const procSaisonhistorie = new PageManager("Saisonhistorie", __TEAMCLASS, () => {
+        const __TEAMPARAMS = getTeamParamsFromTable(__TEAMSEARCHTEAM, __TEAMIDSEARCHTEAM);
+
+        const __ROWOFFSETUPPER = 1;     // Header-Zeile
+        const __COLUMNINDEX = {
+                'Sai' : 0,
+                'Lig' : 1,
+                'Tab' : 2,
+                'Pok' : 3,
+                'OSE' : 4,
+                'OSC' : 5
+            };
+
+        // Fuer den Handler unten merken...
+        this.__ROWOFFSETUPPER = __ROWOFFSETUPPER;
+        this.__COLUMNINDEX = __COLUMNINDEX;
+
+        return {
+                'Tab'         : getElement('table#leihe.sortable'),  // #2: Sortierbare Tabelle, die ID '#leihe' ist bestimmt falsch kopiert!
+                'Zei'         : __ROWOFFSETUPPER,
+                'Spa'         : __COLUMNINDEX.Sai,
+                'teamParams'  : __TEAMPARAMS,
+                'menuAnchor'  : getElement('div'),
+                'hideForm'    : {
+                                    'team'  : true
+                                },
+                'formWidth'   : 3,
+                'formBreak'   : 4
+            };
+    }, async optSet => {
+        //const __ROWOFFSETUPPER  = this.__ROWOFFSETUPPER;    // von oben!
+        //const __ROWOFFSETLOWER  = 0;
+
+        const __ROWS = getRows('table#leihe.sortable');  // #2: Sortierbare Tabelle, die ID '#leihe' ist bestimmt falsch kopiert!
+
+        if (! __ROWS) {
+            __LOG[1]("Keine Saisonhistorie vorhanden!");
+            return;
+        }
+
+        // Format der Trennlinie zwischen den Eintraegen...
+        const __BORDERSTRING = optSet.getOptValue('sepStyle') + ' ' + optSet.getOptValue('sepColor') + ' ' + optSet.getOptValue('sepWidth');
+        const __SHORTKOM = optSet.getOptValue('shortKom');
+        const __EMPTYTEXT = 'Nicht teilgenommen';
+
+        Array.from(__ROWS).forEach(row => {
+                const __CELLS = row.cells;    // Aktuelle Eintraege
+
+                row.style.textAlign = 'center';
+
+                Array.from(__CELLS).forEach(cell => {
+                        cell.style.border = __BORDERSTRING;
+
+                        if (__SHORTKOM && (cell.textContent === __EMPTYTEXT)) {
+                            cell.textContent = '';  // Eintrag irritiert etwas!
+                        }
+                    });
+/*
+                //Array.from(document.querySelector('#leihe').rows[i].cells).forEach(cell => (cell.style.border = __BORDERSTRING));
+*/
+            });
+
+        return true;
+    });
+
+// ==================== Ende Page-Manager fuer zu bearbeitende Seiten ====================
+
+// ==================== Spezialbehandlung der Startparameter ====================
+
+// Callback-Funktion fuer die Behandlung der Optionen und Laden des Benutzermenus
+// Diese Funktion erledigt nur Modifikationen und kann z.B. einfach optSet zurueckgeben!
+// optSet: Platz fuer die gesetzten Optionen
+// optParams: Eventuell notwendige Parameter zur Initialisierung
+// 'hideMenu': Optionen werden zwar geladen und genutzt, tauchen aber nicht im Benutzermenu auf
+// 'Tab': Tabelle mit dem Spielplan
+// 'Zei': Startzeile des Spielplans mit dem ersten ZAT
+// 'Spa': Spalte der Tabellenzelle mit der Spielart (z.B. "Liga : Heim")
+// 'teamParams': Getrennte Daten-Option wird genutzt, hier: Team() mit 'LdNr'/'LgNr' des Erst- bzw. Zweitteams
+// 'menuAnchor': Startpunkt fuer das Optionsmenu auf der Seite
+// 'showForm': Checkliste der auf der Seite sichtbaren Optionen (true fuer sichtbar)
+// 'hideForm': Checkliste der auf der Seite unsichtbaren Optionen (true fuer unsichtbar)
+// 'formWidth': Anzahl der Elemente pro Zeile
+// 'formBreak': Elementnummer des ersten Zeilenumbruchs
+// return Gefuelltes Objekt mit den gesetzten Optionen
+function prepareOptions(optSet, optParams) {
+    // Werte aus der HTML-Seite ermitteln...
+    const __BOXSAISONS = document.getElementsByTagName('option');
+    const __SAISON = getSelectionFromComboBox(__BOXSAISONS, 0, 'Number');
+    const __LIGASIZE = 10; // (optParams.Tab ? getLigaSizeFromSpielplan(optParams.Tab.rows, optParams.Zei, optParams.Spa, optSet.getOptValue('aktuelleSaison')) : undefined);
+
+    // ... und abspeichern...
+    optSet.setOpt('aktuelleSaison', __SAISON, false);
+    optSet.setOpt('ligaSize', __LIGASIZE, false);
+
+    return optSet;
+}
+
+// Callback-Funktion fuer die Ermittlung des richtigen PageManagers
+// page: Die ueber den Selektor ermittelte Seitennummer (hier: nur 10 gueltig)
+// return Der zugehoerige PageManager (hier: 0)
+function setupManager(page) {
+    const __MAIN = this;
+
+    return __MAIN.pageManager[page - 10];
+}
+
+// ==================== Ende Spezialbehandlung der Startparameter ====================
+
+// ==================== Hauptprogramm ====================
+
+// Konfiguration der Callback-Funktionen zum Hauptprogramm...
+const __MAINCONFIG = {
+                        setupManager    : setupManager,
+                        prepareOpt      : prepareOptions
+                    };
+
+// Selektor (Seite bzw. Parameter) fuer den richtigen PageManager...
+const __LEAFS = {
+                    'showteam.php' : 0, // Teamansicht Hauptfenster
+                    'st.php'       : 0  // Teamansicht Popupfenster
+                };
+const __ITEM = 's';
+
+// URL-Legende:
+// s=0: Teamuebersicht
+// s=1: Vertragsdaten
+// s=2: Einzelwerte
+// s=3: Statistik Saison
+// s=4: Statistik Gesamt
+// s=5: Teaminfo
+// s=6: Saisonplan (*) s=6 wird behandelt durch PageManager #0
+// s=7: Vereinshistorie
+// s=8: Transferhistorie
+// s=9: Leihhistorie
+// s=10: Saisonhistorie
+const __MAIN = new Main(__OPTCONFIG, __MAINCONFIG, procSaisonhistorie);
+
+__MAIN.run(getPageIdFromURL, __LEAFS, __ITEM);
+
+// ==================== Ende Hauptprogramm ====================
+
+// *** EOF ***
