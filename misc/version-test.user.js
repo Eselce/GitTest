705e5d97b1b1d3a71de5bcf3ef80412dc544d8cd
SLC <S.Loges@gmx.de>
2023-02-03 01:40:24 +0100

    version-test.user.js: Versionsnummern testen

---
+++ misc/version-test.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// _name         version-test
+// _namespace    http://os.ongapo.com/
+// _version      0.10
+// _copyright    2023+
+// _author       Sven Loges (SLC)
+// _description  Dummy userscript for version tests and updates
+// ==/UserScript==
+
+(() => {
+    // ECMAScript 6:
+    /* jshint esnext: true */
+    /* jshint moz: true */
+
+    return true;
+})();
+
+// *** EOF ***
